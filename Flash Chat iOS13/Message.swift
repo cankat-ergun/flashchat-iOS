70a8dbb27af47d4fcde9b2c58a39e67f0d4c7fd5
Cankat Ergün <80051347+cankat-ergun@users.noreply.github.com>
2022-09-30 15:42:19 +0300

    Add files via upload

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Argun Cankat Ergun on 6.09.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // something@someelse.com
+    let body: String
+}
